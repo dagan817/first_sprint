31fa6c1cba4f5d00292684c78a6035862a02f72e
dagan817 <135942487+dagan817@users.noreply.github.com>
2023-06-13 12:53:51 +0300

    Update setup.py

--- calculator/setup.py
+++ calculator/setup.py
@@ -1,1 +1,20 @@
+from setuptools import setup, find_packages
 
+setup(
+    name='dovydas_calculator',
+    version='1.0.0',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'dovydas_calculator = dovydas_calculator.calculator:main',
+        ],
+    },
+    install_requires=[
+        'pytest',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
