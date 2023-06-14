2da8bf10873dcf9ae9d767575b3cb30f2818f6dc
dagan817 <135942487+dagan817@users.noreply.github.com>
2023-06-14 04:51:26 +0000

    changes

--- dovydas_calculator/stufftsts/whatisthat.py
+++
@@ -1,8 +0,0 @@
-from dovydas_calculator.calculator import add, subtract, multiply, divide, nth_root, Calculator 
-
-calculator = Calculator()
-calculator.store_result(10)
-calculator.add_to_memory(5)
-result = subtract(calculator.recall_memory(), 3)
-calculator.store_result(result)
-assertion = calculator.recall_memory() == 12
